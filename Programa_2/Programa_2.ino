07735b60feabbaa8cc890883a4401e7f71dd2f02
LuisRodriguez2020 <59728900+LuisRodriguez2020@users.noreply.github.com>
2020-01-10 13:15:49 +0100

    Programa 2
    Programa 2

--- Programa_2/Programa_2.ino
+++ Programa_2/Programa_2.ino
@@ -50,7 +50,7 @@ void loop()
   digitalWrite(led3, HIGH);     // posar a 0V el pin 8
   digitalWrite(led4, LOW);     // posar a 0V el pin 9
   digitalWrite(led5, HIGH);     // posar a 0V el pin 10
-  digitalWrite(led6, LOW);     // posar a 0V el pin 11
+  digitalWrite(led6, LOW);     // posar a 0V el pin 110
   digitalWrite(led7, HIGH);     // posar a 0V el pin 12
   
   delay(500);                  // es queden leds 500ms apagats
